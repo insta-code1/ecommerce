4b58cc9e4ca45cf152edf202ea30184ecd89ce81
insta-code1 <eoinkane1@gmail.com>
2016-06-09 19:11:47 +0100

    first commit

---
+++ src/carts/models.py
@@ -0,0 +1,70 @@
+from decimal import Decimal
+from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+
+from products.models import Variation
+# Create your models here.
+
+
+class CartItem(models.Model):
+	cart = models.ForeignKey("Cart")
+	item = models.ForeignKey(Variation)
+	quantity = models.PositiveIntegerField(default=1)
+	line_item_total = models.DecimalField(max_digits=10, decimal_places=2)
+
+	def __unicode__(self):
+		return self.item.title
+
+	def remove(self):
+		return self.item.remove_from_cart()
+
+
+def cart_item_pre_save_receiver(sender, instance, *args, **kwargs):
+	qty = instance.quantity
+	if qty >= 1:
+		price = instance.item.get_price()
+		line_item_total = Decimal(qty) * Decimal(price)
+		instance.line_item_total = line_item_total
+
+pre_save.connect(cart_item_pre_save_receiver, sender=CartItem)
+
+
+
+def cart_item_post_save_reciver(sender, instance, *args, **kwargs):
+	instance.cart.update_subtotal()
+
+post_save.connect(cart_item_post_save_reciver, sender=CartItem)
+
+
+class Cart(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+	items = models.ManyToManyField(Variation, through=CartItem)
+	timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+	updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+	subtotal = models.DecimalField(max_digits=50, decimal_places=2)
+
+	def __unicode__(self):
+		return str(self.id)
+
+
+	def update_subtotal(self):
+		print "updating..."
+		subtotal = 0
+		items = self.cartitem_set.all()
+		for item in items:
+			subtotal += item.line_item_total
+		self.subtotal = subtotal
+		self.save()
+
+	# user
+	# items 
+	# timestamp ** created
+	# updated ** updated
+
+	# subtotal price
+	# taxes total
+	# discounts
+	# total price
