b32f8549bd1c518846a791c85d298db34023a01c
insta-code1 <eoinkane1@gmail.com>
2016-06-17 13:41:31 +0100

    Added order detail page and LoginRequiredMixin @method_decorator(login_required)

--- src/orders/views.py
+++ src/orders/views.py
@@ -1,18 +1,41 @@
 from django.contrib import messages
+from django.http import Http404
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, FormView
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 # Create your views here.
 
 from .forms import AddressForm, UserAddressForm
-from .mixins import CartOrderMixin
+from .mixins import CartOrderMixin, LoginRequiredMixin
 from .models import UserAddress, UserCheckout, Order
 
-class OrderList(ListView):
+
+class OrderDetail(DetailView):
+	model = Order
+
+	def dispatch(self, request, *args, **kwargs):
+		try:
+			user_check_id = self.request.session.get("user_checkout_id")
+			user_checkout = UserCheckout.objects.get(id=user_check_id)
+		except UserCheckout.DoesNotExist:
+			user_checkout = UserCheckout.objects.get(user=request.user)
+		except:
+			user_checkout = None
+
+		obj = self.get_object()
+		if obj.user == user_checkout and user_checkout is not None:
+			return super(OrderDetail, self).dispatch(request, *args, **kwargs)
+		else:
+			raise Http404
+
+
+
+class OrderList(LoginRequiredMixin, ListView):
 	queryset = Order.objects.all()
 
 	def get_queryset(self):
-		user_check_id = self.request.session.get("user_checkout_id")
+		user_check_id = self.request.user.id
 		user_checkout = UserCheckout.objects.get(id=user_check_id)
 		return super(OrderList, self).get_queryset().filter(user=user_checkout)
 
@@ -21,6 +44,8 @@ class UserAddressCreateView(CreateView):
 	template_name = "forms.html"
 	success_url = "/checkout/address/"
 
+
+	#user checlout session below
 	def get_checkout_user(self):
 		user_check_id = self.request.session.get("user_checkout_id")
 		user_checkout = UserCheckout.objects.get(id=user_check_id)
